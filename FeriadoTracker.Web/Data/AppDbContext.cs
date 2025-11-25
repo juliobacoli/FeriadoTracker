ce4bc4b8b18505bac221718c2c5f1efc73797a6d
Julio Bacoli <julio-microlins@hotmail.com>
2025-11-25 01:04:18 -0300

    Configura seed de feriados e atualiza favicon
    Substitui o favicon antigo (`favicon.ico`) por um novo ícone
    em formato SVG (`favicon.svg`) representando um calendário.
    Altera `_Layout.cshtml` para referenciar o novo favicon.

--- FeriadoTracker.Web/Data/AppDbContext.cs
+++ FeriadoTracker.Web/Data/AppDbContext.cs
@@ -11,6 +11,8 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Feriado>().HasData(
+            //Falta adicionar o feriado de carnaval
+            //Falta adicionar o feriado de corpus christi
             new Feriado { Id = 1, Nome = "Natal", Data = new DateTime(2025, 12, 25), Tipo = "Nacional" },
             new Feriado { Id = 2, Nome = "Confraternização Universal", Data = new DateTime(2026, 01, 01), Tipo = "Nacional" },
             new Feriado { Id = 3, Nome = "Sexta-feira Santa", Data = new DateTime(2026, 04, 03), Tipo = "Nacional" },

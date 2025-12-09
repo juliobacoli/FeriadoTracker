7ab26d1040558cedf9df19a606ac5f61b2c46ab7
Julio Bacoli <julio-microlins@hotmail.com>
2025-12-09 00:09:51 -0300

    Adiciona feriados de Carnaval e Corpus Christi em 2026
    Inclui Carnaval (16/02 e 17/02/2026) e Corpus Christi (04/06/2026) na lista de feriados nacionais. Atualiza IDs e datas dos feriados subsequentes. Cria migration `AdicionaCarnavalECorpusChristi` e ajusta banco de dados e configurações para refletir as mudanças.

--- FeriadoTracker.Web/Data/AppDbContext.cs
+++ FeriadoTracker.Web/Data/AppDbContext.cs
@@ -11,19 +11,20 @@ public class AppDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Feriado>().HasData(
-            //Falta adicionar o feriado de carnaval
-            //Falta adicionar o feriado de corpus christi
             new Feriado { Id = 1, Nome = "Natal", Data = new DateTime(2025, 12, 25), Tipo = "Nacional" },
             new Feriado { Id = 2, Nome = "Confraternização Universal", Data = new DateTime(2026, 01, 01), Tipo = "Nacional" },
-            new Feriado { Id = 3, Nome = "Sexta-feira Santa", Data = new DateTime(2026, 04, 03), Tipo = "Nacional" },
-            new Feriado { Id = 4, Nome = "Tiradentes", Data = new DateTime(2026, 04, 21), Tipo = "Nacional" },
-            new Feriado { Id = 5, Nome = "Dia do Trabalho", Data = new DateTime(2026, 05, 01), Tipo = "Nacional" },
-            new Feriado { Id = 6, Nome = "Independência do Brasil", Data = new DateTime(2026, 09, 07), Tipo = "Nacional" },
-            new Feriado { Id = 7, Nome = "Nossa Senhora Aparecida", Data = new DateTime(2026, 10, 12), Tipo = "Nacional" },
-            new Feriado { Id = 8, Nome = "Finados", Data = new DateTime(2026, 11, 02), Tipo = "Nacional" },
-            new Feriado { Id = 9, Nome = "Dia Nacional da Consciência Negra", Data = new DateTime(2026, 11, 20), Tipo = "Nacional" },
-            new Feriado { Id = 10, Nome = "Proclamação da República", Data = new DateTime(2026, 11, 15), Tipo = "Nacional" },
-            new Feriado { Id = 11, Nome = "Natal", Data = new DateTime(2026, 12, 25), Tipo = "Nacional" }
+            new Feriado { Id = 3, Nome = "Carnaval", Data = new DateTime(2026, 02, 16), Tipo = "Nacional" },
+            new Feriado { Id = 4, Nome = "Carnaval", Data = new DateTime(2026, 02, 17), Tipo = "Nacional" },
+            new Feriado { Id = 5, Nome = "Sexta-feira Santa", Data = new DateTime(2026, 04, 03), Tipo = "Nacional" },
+            new Feriado { Id = 6, Nome = "Tiradentes", Data = new DateTime(2026, 04, 21), Tipo = "Nacional" },
+            new Feriado { Id = 7, Nome = "Dia do Trabalho", Data = new DateTime(2026, 05, 01), Tipo = "Nacional" },
+            new Feriado { Id = 8, Nome = "Corpus Christi", Data = new DateTime(2026, 06, 04), Tipo = "Nacional" },
+            new Feriado { Id = 9, Nome = "Independência do Brasil", Data = new DateTime(2026, 09, 07), Tipo = "Nacional" },
+            new Feriado { Id = 10, Nome = "Nossa Senhora Aparecida", Data = new DateTime(2026, 10, 12), Tipo = "Nacional" },
+            new Feriado { Id = 11, Nome = "Finados", Data = new DateTime(2026, 11, 02), Tipo = "Nacional" },
+            new Feriado { Id = 12, Nome = "Proclamação da República", Data = new DateTime(2026, 11, 15), Tipo = "Nacional" },
+            new Feriado { Id = 13, Nome = "Dia Nacional da Consciência Negra", Data = new DateTime(2026, 11, 20), Tipo = "Nacional" },
+            new Feriado { Id = 14, Nome = "Natal", Data = new DateTime(2026, 12, 25), Tipo = "Nacional" }
         );
     }
 }
---
+++ FeriadoTracker.Web/Migrations/20251209030526_AdicionaCarnavalECorpusChristi.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace FeriadoTracker.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaCarnavalECorpusChristi : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 2, 16, 0, 0, 0, 0, DateTimeKind.Unspecified), "Carnaval" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 2, 17, 0, 0, 0, 0, DateTimeKind.Unspecified), "Carnaval" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 4, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sexta-feira Santa" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Tiradentes" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dia do Trabalho" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 6, 4, 0, 0, 0, 0, DateTimeKind.Unspecified), "Corpus Christi" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 9, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Independência do Brasil" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 10, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Nossa Senhora Aparecida" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Finados" });
+
+            migrationBuilder.InsertData(
+                table: "Feriados",
+                columns: new[] { "Id", "Data", "Nome", "Tipo" },
+                values: new object[,]
+                {
+                    { 12, new DateTime(2026, 11, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Proclamação da República", "Nacional" },
+                    { 13, new DateTime(2026, 11, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dia Nacional da Consciência Negra", "Nacional" },
+                    { 14, new DateTime(2026, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Natal", "Nacional" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 4, 3, 0, 0, 0, 0, DateTimeKind.Unspecified), "Sexta-feira Santa" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 4, 21, 0, 0, 0, 0, DateTimeKind.Unspecified), "Tiradentes" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 5, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dia do Trabalho" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 9, 7, 0, 0, 0, 0, DateTimeKind.Unspecified), "Independência do Brasil" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 10, 12, 0, 0, 0, 0, DateTimeKind.Unspecified), "Nossa Senhora Aparecida" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 11, 2, 0, 0, 0, 0, DateTimeKind.Unspecified), "Finados" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 11, 20, 0, 0, 0, 0, DateTimeKind.Unspecified), "Dia Nacional da Consciência Negra" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 11, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), "Proclamação da República" });
+
+            migrationBuilder.UpdateData(
+                table: "Feriados",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Data", "Nome" },
+                values: new object[] { new DateTime(2026, 12, 25, 0, 0, 0, 0, DateTimeKind.Unspecified), "Natal" });
+        }
+    }
+}
